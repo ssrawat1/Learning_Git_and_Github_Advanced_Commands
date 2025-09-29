cc993d5fc84cca47ad80b89a07fb90c27b86efe7
ssrawat1 <ssr911999@gmail.com>
2025-09-29 13:28:11 +0530

    Create a generateFourDigitOTP function in the script.js file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-(function greet(msg){
-  console.log(msg)
-})("Hello World!")
+(function greet(msg) {
+  console.log(msg);
+})('Hello World!');
+
+export function generateFourDigitOtp() {
+  return Math.floor(Math.random() * 9000) + 1000;
+}
