93702baf545b29a45729cd9dedfab3772a0ee1ca
ssrawat1 <ssr911999@gmail.com>
2025-09-29 13:26:22 +0530

    Add the style on the body in the style.css file

---
+++ script.js
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+=======
+(function greet(msg) {
+  console.log(msg);
+})('Hello World!');
+
+export function generateFourDigitOtp() {
+  return Math.floor(Math.random() * 9000) + 1000;
+}
+>>>>>>> Stashed changes
